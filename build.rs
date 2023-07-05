572f7d1158253be545c9099a40b7cdab0f7727a5
Mike Urbach <mikeurbach@gmail.com>
2023-07-04 23:03:34 -0600

    Set the staticlib build type and rerun-if-changed.

--- build.rs
+++ build.rs
@@ -2,4 +2,6 @@ fn main() {
     cxx_build::bridge("src/lib.rs")
         .flag_if_supported("-std=c++17")
         .compile("egg-netlist-synthesizer");
+
+    println!("cargo:rerun-if-changed=src/lib.rs");
 }
